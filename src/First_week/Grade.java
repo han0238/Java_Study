5a1ce9b4bbcffa341c07b864df5144df3e2f0b7e
han0238 <sherlock0238@gmail.com>
2022-05-24 20:57:41 +0900

    test

---
+++ src/First_week/Grade.java
@@ -0,0 +1,19 @@
+package First_week;
+
+public class Grade {
+
+  public static void main(String[] args) {
+
+  }
+
+}
+
+class Student{
+  String name = "";
+  int ban = 0;
+  int no = 0;
+  int kor = 0;
+  int eng = 0;
+
+
+}
